e7855497333570145d4a71e4792ee046864382fd
Matt Mueller <mattmuelle@gmail.com>
2011-10-23 03:19:27 -0700

    Rolling with thimble's static document server to avoid using express's static

--- index.js
+++ index.js
@@ -6,20 +6,32 @@
 var express = require('express'),
 	markdown = require('gh-markdown'),
 	fs = require('fs'),
+	mime = require('mime'),
+	path = require('path'),
 	docs, app;
-	
 	
 app = express.createServer();
 
 docs = [
 	'intro.md',
 	'outro.md'
-
 ];
 
 docs = docs.map(function(doc) {
 	return './docs/' + doc;
-})
+});
+
+app.get('/?assets/*', function(req, res) {
+	var asset = fs.readFileSync("./" + req.url, "utf8"),
+		header;
+
+	if (!res.getHeader("content-type")) {
+	  header = getHeader("blah." + path.extname(req.url));
+	  res.setHeader('Content-Type', header);
+	}
+	
+	res.send(asset);
+});
 
 app.get('/', function(req, res){
 	var intro = fs.readFileSync('./html/intro.html'),
@@ -27,10 +39,17 @@ app.get('/', function(req, res){
 		
 	markdown(docs, function(err, html) {
 		if (err) throw err;
-		
 		html = intro + html + outro;
 		res.send(html);
 	});
 });
+
+function getHeader(assetPath) {
+  var charset, type;
+  type = mime.lookup(assetPath);
+  charset = mime.charsets.lookup(type);
+  charset = charset ? "; charset=" + charset : "";
+  return type + charset;
+}
 
 app.listen(3000);
